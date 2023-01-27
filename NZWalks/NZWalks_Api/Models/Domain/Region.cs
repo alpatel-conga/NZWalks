f944ffa0763f0cb9b6a87ee0b9b6e0ca9ebebcd2
alpatel2002 <alpatel@conga.com>
2023-01-27 09:49:50 +0530

    Initial files

---
+++ NZWalks/NZWalks_Api/Models/Domain/Region.cs
@@ -0,0 +1,19 @@
+﻿namespace NZWalks_Api.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Long { get; set; }
+
+        public long population { get; set; }
+    }
+}
